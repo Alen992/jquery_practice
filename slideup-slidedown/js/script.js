2e62cfc0b3b6d9e1d45b65064c1c2907c62099fa
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 09:48:57 +0100

    slidedown, slideup, slideToggle

---
+++ slideup-slidedown/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+$('.green-box').slideUp(2000);
+$('.green-box').slideDown(2000);
+$('p').slideUp(2000);
+$('p').slideDown(2000);
+
+$('.blue-box').slideToggle(2000);
+$('.blue-box').slideToggle(2000);
+});
