17399072a21b2ea6f7d1923aac9cd0e30b290fff
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 13:37:29 +0100

    replaceALl

--- 17- replacing-elements-content/js/script.js
+++ 17- replacing-elements-content/js/script.js
@@ -5,5 +5,6 @@ $(function() {
   // })
   // const firstListItem = $('li:first');
   // $('p:first').replaceWith(firstListItem);
-  $('.red-box, .blue-box').replaceWith($('<div class="green-box">green box 1</div>'));
+  // $('.red-box, .blue-box').replaceWith($('<div class="green-box">green box 1</div>'));
+  $('<div class="green-box">green box 1. More green!</div>').replaceAll('.red-box, .blue-box')
 });
