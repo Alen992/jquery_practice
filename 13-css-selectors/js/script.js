71b923f284766ae90e4eeb1bbf622632984c22e6
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:03:21 +0100

    css selectors: odd/even

--- 13-css-selectors/js/script.js
+++ 13-css-selectors/js/script.js
@@ -6,4 +6,6 @@ $(function() {
 //  $('h2, p, input').css('background-color', 'rgba(180, 180, 30, .8)');
 $('p:first').css('background-color', 'rgba(180, 180, 30, .8)');
 $('li:last').css('background-color', 'rgba(80,130,30, .3)');
+$('li:odd').css('background-color', 'red');
+$('li:even').css('background-color', 'lightblue');
 });
