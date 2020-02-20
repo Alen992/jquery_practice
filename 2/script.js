03621b9b462efec551da6a03240dec325fca834d
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 08:45:19 +0100

    commit

--- 2/script.js
+++ 2/script.js
@@ -1,4 +1,5 @@
 $('document').ready(function() {
     $('body').css({'background-color': 'lightgrey', 'text-align': 'center'})
     .prepend('<h1>Hello</h1>');
+    console.log('document is ok and ready!')
 })
