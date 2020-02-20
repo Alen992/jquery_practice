0c8f977aae30bb91594ebffb5eac0c79ac2a602d
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 13:51:49 +0100

    jQuery removing elements: finished

---
+++ 18-remove-elements/js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+  // const removeListItem = $('li').remove();
+  // $('form').children().not('textarea, input[type=text], br').remove();
+
+  // const detachedListItem = $('li').detach();
+  // console.log(detachedListItem);
+  // $('#content').append(detachedListItem);
+
+$('p:first').empty();
+
+$('.red-box, .green-box, .blue-box ').empty();
+});
