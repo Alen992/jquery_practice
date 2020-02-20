6d115dc66dbe7f9aa28df28b086893050c023eaa
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 10:53:32 +0100

    ordered files and started jquery css selectors

---
+++ 13-css-selectors/js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+//  $('p').css('background-color', 'rgba(180, 180, 30, .8)');
+//  $('.red-box').css('background-color', 'rgba(180, 180, 30, .8)');
+ $('#list').css('background-color', 'rgba(180, 180, 30, .8)');
+});
