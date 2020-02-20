25b3b0ae1a5553134b99693a7a0e004c3393606a
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 14:04:47 +0100

    jQuery attr

--- 19-manipulate-attribute-properties/js/script.js
+++ 19-manipulate-attribute-properties/js/script.js
@@ -1,3 +1,9 @@
 $(function() {
-  
+  // attr(), prop(), val()
+  const specialLink = $('#special-link');
+  console.log(specialLink.attr('href'));
+  console.log(specialLink.attr('title'));
+
+  specialLink.attr('href', 'http://petersommerhoff.com')
+  console.log(specialLink.attr('href'));
 });
