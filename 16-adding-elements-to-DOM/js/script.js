36908999c9f50ea4ebfdfea2887f4f38e8ed5cf6
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:51:56 +0100

    after(), before()

--- 16-adding-elements-to-DOM/js/script.js
+++ 16-adding-elements-to-DOM/js/script.js
@@ -1,7 +1,12 @@
 $(function() {
-  $('ul ul:first').append('<li>I\'m gonna be the last sub item</li>');
-  $('<li>I\'m gonna be the last item</li>').appendTo($('ul ul:last'));
+  // $('ul ul:first').append('<li>I\'m gonna be the last sub item</li>');
+  // $('<li>I\'m gonna be the last item</li>').appendTo($('ul ul:last'));
 
-  $('ul ul:first').prepend('<li>I\'m gonna be the first sub item</li>');
-  $('<li>I\'m gonna be the first sub item</li>').prependTo('ul ul:last');
+  // $('ul ul:first').prepend('<li>I\'m gonna be the first sub item</li>');
+  // $('<li>I\'m gonna be the first sub item</li>').prependTo('ul ul:last');
+
+  // $('<h4>Peter Somehoff</h4>').prependTo('#content');
+
+  $('.red-box').after('<div class="red-box">Another Red</div>');
+  $('.green-box').before('<div class="green-box">Another green</div>');
 });
