ab609d6bba6216ec7ed17c26fe8aa820e34da242
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 14:18:32 +0100

    commit

--- 19-manipulate-attribute-properties/js/script.js
+++ 19-manipulate-attribute-properties/js/script.js
@@ -1,9 +1,34 @@
 $(function() {
   // attr(), prop(), val()
-  const specialLink = $('#special-link');
-  console.log(specialLink.attr('href'));
-  console.log(specialLink.attr('title'));
+  // const specialLink = $('#special-link');
+  // console.log(specialLink.attr('href'));
+  // console.log(specialLink.attr('title'));
 
-  specialLink.attr('href', 'http://petersommerhoff.com')
-  console.log(specialLink.attr('href'));
+  // specialLink.attr('href', 'http://petersommerhoff.com')
+  // console.log(specialLink.attr('href'));
+
+  // const checkbox = $('input:checkbox');
+  // if(checkbox.prop('checked')) {
+  //   console.log('it is checked!');
+  // }
+  // else {
+  //   console.log('It is not checked!');
+    
+  // }
+
+  // $('.submit').click(function() {
+  //   if(checkbox.prop('checked')) {
+  //     console.log('it is checked!');
+  //   }
+  //   else {
+  //     console.log('It is not checked!');
+      
+  //   }
+  // })
+
+  // console.log(checkbox.attr('checked'));
+
+  const textInput = $('input:text');
+  console.log(textInput.val());
+  
 });
