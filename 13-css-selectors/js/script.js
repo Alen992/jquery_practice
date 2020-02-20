6b24697b1e4bcc5addcd4be5df0c9e74599649c4
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:00:11 +0100

    commit

--- 13-css-selectors/js/script.js
+++ 13-css-selectors/js/script.js
@@ -1,5 +1,7 @@
 $(function() {
 //  $('p').css('background-color', 'rgba(180, 180, 30, .8)');
 //  $('.red-box').css('background-color', 'rgba(180, 180, 30, .8)');
- $('#list').css('background-color', 'rgba(180, 180, 30, .8)');
+//  $('#list').css('background-color', 'rgba(180, 180, 30, .8)');
+//  $('input[type="text"]').css('background-color', 'rgba(180, 180, 30, .8)');
+ $('h2, p, input').css('background-color', 'rgba(180, 180, 30, .8)');
 });
