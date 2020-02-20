ff530c180641ea9244f8c8dc38b41034b47e61f5
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 10:13:54 +0100

    delay-chaining animations

--- custom-animation/js/script.js
+++ custom-animation/js/script.js
@@ -1,5 +1,22 @@
 $(function() {
-$('.blue-box').animate({
-    'margin-left': '+=200px'
-    }, 2000);
+    $('.blue-box').animate({
+    'margin-left': '+=200px',
+    'opacity': '0',
+    'height': '50px',
+    'width': '50px',
+    'margin-top': '25px'
+    }, 2000, 'linear');
+    $('.blue-box').animate({
+        'margin-left': '-=200px',
+        'opacity': '1',
+        'height': '80px',
+        'width': '80px',
+        'margin-top': '10px'
+    }, 2000, 'linear');
+    $('p').animate({
+        'font-size': '20px'
+    }, 1000);
+    $('p').animate({
+        'font-size': '16px'
+    }, 1000);
 });
---
+++ delay-chain-animations/js/script.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('.red-box').fadeTo(1000, .2);
+  $('.green-box').delay(1000).fadeTo(1000, .5);
+  $('.blue-box').delay(2000).fadeTo(1000, .8).delay(1000).fadeOut().delay(1000).fadeIn();
+});
