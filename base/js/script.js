03a7600ed2e7a4dde05f5fe26b133f0dcc7c721a
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 09:23:39 +0100

    fadeToggle

--- base/js/script.js
+++ base/js/script.js
@@ -5,4 +5,7 @@ $(function() {
   $('.red-box').fadeTo( 2000, .2);
   $('.green-box').fadeTo( 2000, .5);
   $('.blue-box').fadeTo(2000, .8);
+
+  $('.blue-box').fadeToggle();
+  $('.blue-box').fadeToggle();
 });
