6bb2835342e70fbddcd0da1ea52486000bf913d3
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:01:43 +0100

    css selectors

--- 13-css-selectors/js/script.js
+++ 13-css-selectors/js/script.js
@@ -3,5 +3,7 @@ $(function() {
 //  $('.red-box').css('background-color', 'rgba(180, 180, 30, .8)');
 //  $('#list').css('background-color', 'rgba(180, 180, 30, .8)');
 //  $('input[type="text"]').css('background-color', 'rgba(180, 180, 30, .8)');
- $('h2, p, input').css('background-color', 'rgba(180, 180, 30, .8)');
+//  $('h2, p, input').css('background-color', 'rgba(180, 180, 30, .8)');
+$('p:first').css('background-color', 'rgba(180, 180, 30, .8)');
+$('li:last').css('background-color', 'rgba(80,130,30, .3)');
 });
