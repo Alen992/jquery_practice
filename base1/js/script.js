b28813110f96dc10983fe9c23ae01119c5a9e516
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 09:32:58 +0100

    commit

--- base1/js/script.js
+++ base1/js/script.js
@@ -1,5 +1,8 @@
 $(function() {
   $('.green-box').fadeTo(2000, 0);
-
-
+  $('.green-box').fadeTo(2000, .5);
+  $('.red-box').fadeOut(2000);
+  $('.red-box').fadeIn(2000, .4);
+  $('.blue-box').fadeOut(2000);
+  $('.blue-box').fadeIn(2000, .4);
 });
