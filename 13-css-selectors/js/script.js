33e12104dfc48460e4484904934d10cc9e66bf6b
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:06:27 +0100

    finished css selectors

--- 13-css-selectors/js/script.js
+++ 13-css-selectors/js/script.js
@@ -4,8 +4,14 @@ $(function() {
 //  $('#list').css('background-color', 'rgba(180, 180, 30, .8)');
 //  $('input[type="text"]').css('background-color', 'rgba(180, 180, 30, .8)');
 //  $('h2, p, input').css('background-color', 'rgba(180, 180, 30, .8)');
-$('p:first').css('background-color', 'rgba(180, 180, 30, .8)');
-$('li:last').css('background-color', 'rgba(80,130,30, .3)');
-$('li:odd').css('background-color', 'red');
-$('li:even').css('background-color', 'lightblue');
+// $('p:first').css('background-color', 'rgba(180, 180, 30, .8)');
+// $('li:last').css('background-color', 'rgba(80,130,30, .3)');
+// $('li:odd').css('background-color', 'red');
+// $('li:even').css('background-color', 'lightblue');
+
+  $('input:text').css('background-color', 'rgba(180, 180, 30, .8)')
+// $('input:checkbox').css('background-color', 'rgba(180, 180, 30, .8)')
+// $('input:radio').css('background-color', 'rgba(180, 180, 30, .8)')
+// $('input:selected').css('background-color', 'rgba(180, 180, 30, .8)')
+// $('input:checked').css('background-color', 'rgba(180, 180, 30, .8)')
 });
