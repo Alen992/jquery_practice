5d7a6629b4e6b2d131f1bc0f6f7a7fe8293efc40
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 13:35:50 +0100

    replaceWith

--- 17- replacing-elements-content/js/script.js
+++ 17- replacing-elements-content/js/script.js
@@ -3,6 +3,7 @@ $(function() {
   // $('li').replaceWith(function() {
   //   return '<li>Replaced with function</li>';
   // })
-  const firstListItem = $('li:first');
-  $('p:first').replaceWith(firstListItem);
+  // const firstListItem = $('li:first');
+  // $('p:first').replaceWith(firstListItem);
+  $('.red-box, .blue-box').replaceWith($('<div class="green-box">green box 1</div>'));
 });
