7338e46c038ca720675c16fa62bb2c416be40085
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 09:21:02 +0100

    fadeOut, fadeIn, fadeTo

--- base/js/script.js
+++ base/js/script.js
@@ -2,5 +2,7 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $(".red-box").fadeOut(2000);
+  $('.red-box').fadeTo( 2000, .2);
+  $('.green-box').fadeTo( 2000, .5);
+  $('.blue-box').fadeTo(2000, .8);
 });
