97a1510ea7665eb738dfec09b365affc65f9a880
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-20 11:35:41 +0100

    jquery filte: finished

--- 15-jquery-filtering/js/script.js
+++ 15-jquery-filtering/js/script.js
@@ -1,3 +1,18 @@
 $(function() {
+// $('#list').find('li').filter(':even').css('background-color', 'lightgreen');
+// $('#list').children('li').filter(':even').css('background-color', 'red');
+// $('li').filter(function(index) {
+//   return index % 3 === 2;
+// }).css('background-color', 'lightgreen');
 
+// $('li').first().css('background-color', 'lightgreen');
+// $('li').last().css('background-color', 'orangered');
+// $('li').eq(4).css('background-color', 'orangered');
+// $('li').eq(-2).css('background-color', 'yellow');
+
+// $('li').not(':first').css('background-color', 'lightgreen');
+// $('li').not('#list ul li').css('background-color', 'blue');
+  $('li').not(function(index) {
+    return index % 3 === 2;
+  }).css('background-color', 'purple');
 });
