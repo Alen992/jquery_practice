904441e6c42f667c9eab20d46c5f982ab7db8f12
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-21 11:46:18 +0100

    finished passing-data-to-events

---
+++ 31-passing-data-to-events/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('#btn-click').click({
+    user: 'Peter',
+    domain: 'petersomehoff.com'
+  }, function(event) {
+    greetUser(event.data);
+  });
+
+  function greetUser(userdata) {
+    username = userdata.user || 'Anonymous';
+    domain = userdata.domain || 'example.com';
+
+    alert(' Welcome back ' + username + ' from ' + domain + '!');
+  }1
+}); 
+
