e73d84c536117eab71a654babf84aeb69aac4482
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-21 09:35:53 +0100

    finished click-events

--- 25-click-events/js/script.js
+++ 25-click-events/js/script.js
@@ -1,4 +1,9 @@
 $(function() {
+  $('#btn-click').click(function() {
+    alert('Button was clicked.');
+  });
 
-  
+  $('.red-box').click(function() {
+    $('.red-box').fadeTo(2000, .15);
+  });
 }); 
