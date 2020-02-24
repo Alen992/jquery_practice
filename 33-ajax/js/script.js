4f30a74199d02246dc935464e73e853814ccd6fe
Darko Kaiser <darko.kaiser@etondigital.com>
2020-02-24 12:58:14 +0100

    commit

--- 33-ajax/js/script.js
+++ 33-ajax/js/script.js
@@ -1,26 +1,13 @@
 $(function() {
-  var galleryItems = $('.gallery').find('img');
+  $.get(), $.post(), $().ajax(), $.getJSON()
+  $.load()
+  $('#code').load('js/script.js')
 
-  galleryItems.css({'width': '30%', 'opacity': '.7'});
-
-  galleryItems.mouseenter(function() {
-    $(this).stop().fadeTo(500, 1);
-  });
-
-  galleryItems.mouseleave(function() {
-    $(this).stop().fadeTo(500, .7);
-  })
-
-  galleryItems.click(function() {
-    var source = $(this).attr('src');
-    console.log(source);
-    var image = $('<img>').addClass('newImg').attr('src', source).css('width', '100%');
-    $('.lightbox').empty().append(image).fadeIn(2000);
-  })
-
-$('.lightbox').not('.arrow').click(function() {
-  $(this).css('display', 'none').html('');
-})
-
+  $('#code').load('js/script.js', function(response, status) {
+    if (status == 'error') {
+      alert ('Could not find file!')
+    }
+    console.log(response);
+  }) 
 }); 
 
