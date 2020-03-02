19c1b6c33e00a2393690facf17db1ef10ed9f25c
Darko Kaiser <darko.kaiser@etondigital.com>
2020-03-02 10:36:40 +0100

    finished form expanding

--- practice/form-expanding-search/js/script.js
+++ practice/form-expanding-search/js/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
+  // $('#form').submit(function() {  
+    
+  //   if ( $('.input').hasClass('activeForm')) {
+  //     if($('.input').val().trim() == '') {
+  //       $('.input').removeClass('activeForm');
+  //       return false;
+  //     } else {
+  //       return true;
+  //     }
+  //   } else {
+  //     $('.input').addClass('activeForm');
+  //   }
+
+    
+  // })
+
+
+
+  $('#form').submit(function() {
+    if($('.input').hasClass('activeForm')) {
+      if ($.trim($('.input').val()) === '' ) {
+        $('.input').removeClass('activeForm');
+          return false;
+      } 
+    } else {
+      $('.input').addClass('activeForm');
+      return false;
+    }
+});
   
 });
